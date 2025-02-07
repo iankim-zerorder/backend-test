8482ecf5a6a2c6a2716696b7fa384503a4984f09
iankim-zerorer <ian@zerorder.kr>
2025-02-07 13:32:10 +0900

    update: 답안파일 추가가

---
+++ test.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] performance) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+function solution(performance) {
+    var answer = 0;
+    return answer;
+}
